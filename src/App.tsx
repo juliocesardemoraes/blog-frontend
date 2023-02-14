45311af4a643911bdfcdc2f6943e0cec0b7bade5
julio cesar <jcmcf2012@gmail.com>
2023-02-14 11:16:43 -0300

    feat: add jsdoc

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,19 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Main from "./components/Xpages/main";
 import "./global.css";
 
+/**
+ * Main application file
+ * @return {JSX.Element}
+ */
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Main />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
